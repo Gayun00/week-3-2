da54ea05c4f49a6751d9950b968a48fd188f1ef7
gy <gayunlee11@gmail.com>
2022-02-17 23:42:05 +0900

    Feat: 검색 창 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
+import * as S from '../styles/home/styled';
 
 export default function Home() {
-  const [text, setText] = useState<string>('자바스크립트');
-
-  setTimeout(() => {
-    // 이 부분 주석
-    // setText(0);
-    // 이 부분 주석 해제
-    setText('타입스크립트');
-  }, 1000);
-
   return (
-    <div className="container">
-      <div>
-        <span>{text} 적용 완료</span>
-      </div>
-    </div>
+    <S.Wrapper>
+      {/* <S.SearchInput>searchInput</S.SearchInput> */}
+      <S.SearchResultListContainer>
+        <S.SearchResultList>
+          <S.SearchResultItem>item</S.SearchResultItem>
+          <S.QuickButton>button</S.QuickButton>
+        </S.SearchResultList>
+      </S.SearchResultListContainer>
+    </S.Wrapper>
   );
 }
---
+++ styles/home/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const SearchInput = styled.input``;
+
+export const SearchResultListContainer = styled.div``;
+
+export const SearchResultList = styled.ul``;
+
+export const SearchResultItem = styled.li``;
+
+export const QuickButton = styled.button``;
